9c73a392ffb4dd12b59cd6eefc4907c759f27e56
Boswell Kyle Orendain <boskyle.orendain@gmail.com>
2025-07-30 22:00:25 -0400

    local dev setup complete

--- run-dev.sh
+++ run-dev.sh
@@ -2,9 +2,8 @@
 
 COMPOSE_PATH="./docker/docker-compose.yml"
 
-echo "🔧 Stopping any existing DEV container..."
-docker compose -f "$COMPOSE_PATH" down
+echo "🔧 Stopping and cleaning up any existing DEV containers, volumes, and orphans..."
+docker compose -f "$COMPOSE_PATH" down --volumes --remove-orphans
 
-echo "🔧 Rebuilding and starting DEV container (hot reload)..."
+echo "🚀 Rebuilding and starting DEV container (with hot reload)..."
 docker compose -f "$COMPOSE_PATH" up --build
-
