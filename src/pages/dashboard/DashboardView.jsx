6eb7c412375c31e6b40b1af048e5feb28a5e3767
Boswell Kyle Orendain <boskyle.orendain@gmail.com>
2025-08-04 00:44:14 -0400

    navbar added

--- src/pages/dashboard/DashboardView.jsx
+++ src/pages/dashboard/DashboardView.jsx
@@ -1,44 +1,32 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
-const DashboardView = ({ onLogout, user }) => {
+const DashboardView = ({ user }) => {
   return (
-    <div className="min-h-screen bg-gradient-to-t from-green-900 to-gray-800 p-6  text-offwhite">
-      <div className="max-w-7xl mx-auto">
-        {/* Page Header + Logout Button */}
-        <div className="mb-6 flex justify-between items-center">
-          <div>
-            <h1 className="text-3xl font-bold text-gray-800">Dashboard</h1>
-            <p className="text-gray-600">
-              {/* Welcome back {JSON.stringify(user?.email || "unkown")} */}
-            </p>
-          </div>
-          <button
-            onClick={onLogout}
-            className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
-          >
-            Logout
-          </button>
+    <div className="min-h-screen bg-gradient-to-t from-green-900 to-gray-800 p-6 text-offwhite">
+      <div className="max-w-7xl mx-auto space-y-10">
+        {/* Header */}
+        <div>
+          <h1 className="text-4xl font-bold mb-2">
+            Welcome, {user?.email || "Admin"} 👋
+          </h1>
+          <p className="text-lg text-gray-300">
+            Here’s an overview of the anima cms...
+          </p>
         </div>
 
-        {/* Quick Stats */}
-        <h2 className="mb-10 text-4xl font-bold">Manage Resources</h2>
+        {/* Quick Info Grid */}
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          <Link
-            to="/regions"
-            className="text-2xl
-             
-            font-semibold 
+          <div className="bg-[#333d55] rounded p-4 shadow">
+            <h3 className="text-lg font-semibold mb-1">Total Regions</h3>
+          </div>
 
-            block px-4 
-            py-3 
-          bg-[#333d55] 
-            rounded 
-            shadow 
-            transition"
-          >
-            Regions
-          </Link>
+          <div className="bg-[#333d55] rounded p-4 shadow">
+            <h3 className="text-lg font-semibold mb-1">Last Updated</h3>
+          </div>
+
+          <div className="bg-[#333d55] rounded p-4 shadow">
+            <h3 className="text-lg font-semibold mb-1">Latest Patch</h3>
+          </div>
         </div>
       </div>
     </div>
